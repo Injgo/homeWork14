1110fe2465307bf29145ebbc1f806dcbf37f2e8c
Davit Injgia <dato.injgia18@gmail.com>
2022-07-05 16:38:52 +0400

    new

---
+++ homeWork13/RegistrationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  RegistrationViewController.swift
+//  homeWork13
+//
+//  Created by Dato on 05.07.22.
+//
+
+import UIKit
+
+
+class RegistrationViewController: UIViewController {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UITextField!
+    
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    @IBOutlet var passwordLabel: UIView!
+    
+    @IBOutlet weak var locationLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+}
